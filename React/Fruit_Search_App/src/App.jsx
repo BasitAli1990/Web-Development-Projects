7e2c5fcdec091b25b7030778699021ff191909d5
Basit Ali <163411068+BasitAli1990@users.noreply.github.com>
2026-08-08 17:03:39 +0500

    Fruit Search App complete project
    React Basic practice project

---
+++ React/Fruit_Search_App/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { FruitSearchApp } from './assets/FruitSearchApp'
+
+function App() {
+
+  return (
+    <>
+      <FruitSearchApp/>
+    </>
+  )
+}
+
+export default App
